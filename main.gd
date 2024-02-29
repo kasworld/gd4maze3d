733b7f95cb659375289186792591a2eec59e4ec9
SeukWon Kang <kasworld@gmail.com>
2024-02-29 17:22:43 +0900

    move thread unsafe code

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ extends Node3D
 const PlayerCount = 10
 const VisibleStoreyUp :int = 3
 const VisibleStoreyDown :int = 3
-var maze_size = Vector2i(16*2,9*2)
+var maze_size = Vector2i(16*1,9*1)
 var storey_h :float = 3.0
 var lane_w :float = 4.0
 var wall_thick :float = lane_w *0.05
@@ -29,6 +29,8 @@ func get_cur_storey()->Storey:
 # thread unsafe
 func add_new_storey(stnum :int, msize :Vector2i, h :float, lw :float, wt :float)->void:
 	var st = new_storey(stnum,msize,h,lw,wt)
+	if stnum > 0 :
+		st.start_pos = storey_list[-1].goal_pos
 	storey_list.append(st)
 	add_child(st)
 
@@ -36,8 +38,6 @@ func add_new_storey(stnum :int, msize :Vector2i, h :float, lw :float, wt :float)
 func new_storey(stnum :int, msize :Vector2i, h :float, lw :float, wt :float)->Storey:
 	var gp = rand_pos()
 	var stp = rand_pos()
-	if stnum > 0 :
-		stp = storey_list[-1].goal_pos
 	var st = storey_scene.instantiate()
 	st.init(stnum, msize, h, lw, wt, stp, gp)
 	st.view_floor_ceiling(false,false)

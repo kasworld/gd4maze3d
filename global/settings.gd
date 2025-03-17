775f0a5f7f8fbd9a872e9decaec38d0fbfe871aa
SeukWon Kang <kasworld@gmail.com>
2025-03-17 10:21:46 +0900

    fix camera move

--- global/settings.gd
+++ global/settings.gd
@@ -12,7 +12,6 @@ const LaneW :float = 4.0
 const WallThick :float = LaneW *0.05
 const InterStoreyH :float = StoryH / 100
 
-const TotalH :float = (VisibleStoreyUp+VisibleStoreyDown+1)*(StoryH+InterStoreyH)
 const TotalX :float = MazeSize.x * LaneW
 const TotalY :float = MazeSize.y * LaneW
 

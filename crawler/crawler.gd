57c4f9ee78800be9f7c5b63efa1067925e5ff5fb
seukwonkang <kasworld@gmail.com>
2025-10-13 11:34:33 +0900

    update shape make

--- crawler/crawler.gd
+++ crawler/crawler.gd
@@ -103,26 +103,15 @@ func init(walk_type :EnumWalk.Walk, n :int, LaneW:float,co :Color) -> Crawler:
 		"Args":[],
 	}
 	queue_init()
-	add_shape(n,LaneW,co)
-	return self
-
-func add_shape(n :int, LaneW:float,co :Color) -> Crawler:
 	serial = n
 	color = co
-	var mat = StandardMaterial3D.new()
-	mat.albedo_color = co
-	var mesh = CylinderMesh.new()
-	mesh.height = 0.2*LaneW
-	mesh.top_radius = 0.01*LaneW
-	mesh.bottom_radius = 0.07*LaneW
-	mesh.radial_segments = 5
-	mesh.material = mat
-	var mi3d = MeshInstance3D.new()
-	mi3d.mesh = mesh
-	mi3d.rotation.x = -PI/2
-	mi3d.scale.x = 0.5
-	mi3d.position.x = LaneW*0.2
-	add_child(mi3d)
+	$MeshInstance3D.mesh.material.albedo_color = co
+	$MeshInstance3D.mesh.height = 0.2*LaneW
+	$MeshInstance3D.mesh.top_radius = 0.01*LaneW
+	$MeshInstance3D.mesh.bottom_radius = 0.07*LaneW
+	$MeshInstance3D.rotation.x = -PI/2
+	$MeshInstance3D.scale.x = 0.5
+	$MeshInstance3D.position.x = LaneW*0.2
 	return self
 
 # return true on new act

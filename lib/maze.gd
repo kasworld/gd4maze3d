602572947abedf86b3c8ddad866fffd1471304a2
seukwonkang <kasworld@gmail.com>
2025-10-27 10:52:03 +0900

    update maze

--- lib/maze.gd
+++ lib/maze.gd
@@ -4,23 +4,25 @@ class_name Maze
 var _cells : Array[PackedInt32Array]
 var _maze_size : Vector2i
 
-var visted_pos : Array[Vector2i]
-func select_visited() -> int:
+func _select_visited(visted_pos :Array) -> int:
 	if randi_range(0,1)==0:
 		return visted_pos.size()-1
 	else:
 		return randi_range(0,visted_pos.size()-1)
+
+func _open_dir_at(x:int,y:int, d :int) -> void:
+	_cells[y][x] |= d
 
 func _init(msize :Vector2i) -> void:
 	_maze_size = msize
 	_cells.resize(_maze_size.y)
 	for cl in _cells:
 		cl.resize(_maze_size.x)
-	visted_pos =[]
+	var visted_pos := []
 	var pos = Vector2i( randi_range(0,_maze_size.x-1),randi_range(0,_maze_size.y-1),)
 	visted_pos.append(pos)
 	while visted_pos.size() > 0:
-		var posidx = select_visited()
+		var posidx = _select_visited(visted_pos)
 		pos = visted_pos[posidx]
 		var delpos = true
 		var rnddir = [EnumDir.Flag.North,EnumDir.Flag.South,EnumDir.Flag.East,EnumDir.Flag.West]
@@ -28,8 +30,8 @@ func _init(msize :Vector2i) -> void:
 		for dir in rnddir:
 			var npos = pos + EnumDir.Flag2Vt[dir]
 			if is_in(npos.x,npos.y) && get_cell(npos.x,npos.y)==0:
-				open_dir_at(pos.x,pos.y, dir)
-				open_dir_at(npos.x,npos.y, EnumDir.FlagOpppsite[dir])
+				_open_dir_at(pos.x,pos.y, dir)
+				_open_dir_at(npos.x,npos.y, EnumDir.FlagOpppsite[dir])
 				visted_pos.append(npos)
 				delpos = false
 				break
@@ -41,9 +43,6 @@ func is_in(x:int,y:int) -> bool:
 
 func get_cell(x :int, y:int) -> int:
 	return _cells[y][x]
-
-func open_dir_at(x:int,y:int, d :int) -> void:
-	_cells[y][x] |= d
 
 func is_open_dir_at(x :int, y :int, dir :EnumDir.Flag) -> bool:
 	return (_cells[y][x] & dir) != 0

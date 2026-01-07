75948625ba9648ef66276eb4b7ee9dcf80466414
seukwonkang <kasworld@gmail.com>
2026-01-07 16:06:09 +0900

    update deco orbit

--- deco_orbit.gd
+++ deco_orbit.gd
@@ -1,43 +1,43 @@
 extends Node3D
 
-static var redcolor := NamedColorList.make_red_color_list()
-static var greencolor := NamedColorList.make_green_color_list()
-static var bluecolor := NamedColorList.make_blue_color_list()
-
-var orbitsphere_scene = preload("res://orbit_sphere/orbit_sphere.tscn")
-var diagonal_length :float
-func init(dl :float) -> void:
-	diagonal_length = dl
+var WorldSize :Vector3
+var orbitsphere_list :Array
+func init(WorldSize_a :Vector3, count :int) -> void:
+	WorldSize = WorldSize_a
+	for i in count:
+		add_orbitsphere(i, count)
+func add_orbitsphere(i :int, count :int) -> void:
+	var rate := float(i)/float(count-1) * 0.5 + 0.5
+	var diagonal_length := WorldSize.length() * rate
 	var a120 := PI*2/3
 	var a30 := PI/6
-	var axis1 := Vector3.UP.rotated(Vector3.RIGHT, a30)
-	var axis2 := Vector3.UP.rotated(Vector3.RIGHT.rotated(Vector3.UP,a120), a30)
-	var axis3 := Vector3.UP.rotated(Vector3.RIGHT.rotated(Vector3.UP,a120*2), a30)
-	$Sun.궤도설정(diagonal_length*1.1, 1.0/3, axis1, a120*2
-		).구설정(5, 1, Vector3.UP
-		).구재질설정( preload("res://earthmoon/sun_mat.tres")
-		#).구재질설정( get_color_mat( redcolor.pick_random()[0])
-		).궤도재질설정( get_color_mat(redcolor.pick_random()[0]) )
-	$Earth.궤도설정(diagonal_length, 1.0/2, axis2, 0
-		).구설정(4, 1, Vector3.UP
-		).구재질설정( preload("res://earthmoon/earth_mat.tres")
-		#).구재질설정( get_color_mat(bluecolor.pick_random()[0])
-		).궤도재질설정( get_color_mat(Color.RED) )
-	$Moon.궤도설정(diagonal_length*0.9, 1.0/1, axis3, a120
-		).구설정(3, 1, Vector3.UP
-		).구재질설정( preload("res://earthmoon/moon_mat.tres")
-		#).구재질설정( get_color_mat(greencolor.pick_random()[0])
-		).궤도재질설정( get_color_mat(Color.YELLOW))
-
-func get_color_mat(co: Color)->Material:
-	var mat := StandardMaterial3D.new()
-	mat.albedo_color = co
-	#mat.metallic = 1
-	#mat.clearcoat = true
-	return mat
+	var axis1 := Vector3.UP.rotated(
+		[Vector3.RIGHT, Vector3.LEFT, Vector3.FORWARD, Vector3.BACK].pick_random(),
+		a30)
+	var 궤도mat1 := StandardMaterial3D.new()
+	궤도mat1.albedo_color = random_color()
+	var 구mat2
+	match i:
+		0,1,2:
+			구mat2 = [
+				preload("res://earthmoon/sun_mat.tres"),
+				preload("res://earthmoon/earth_mat.tres"),
+				preload("res://earthmoon/moon_mat.tres"),
+				][i]
+		_:
+			구mat2 = StandardMaterial3D.new()
+			구mat2.albedo_color = random_color()
+	var os = preload("res://orbit_sphere/orbit_sphere.tscn").instantiate(
+		).궤도설정(diagonal_length, diagonal_length/200, axis1, a120*[0,1,2].pick_random()
+		).구설정(WorldSize.x/400*i+1, WorldSize.x/50, Vector3.UP
+		).구재질설정(구mat2).궤도재질설정(궤도mat1)
+	add_child(os)
+	orbitsphere_list.append(os)
 
 func _process(delta: float) -> void:
 	var now := Time.get_unix_time_from_system()
-	$Moon.animate_rotate(now, delta)
-	$Earth.animate_rotate(now, delta)
-	$Sun.animate_rotate(now, delta)
+	for os in orbitsphere_list:
+		os.animate_rotate(now, delta)
+
+func random_color() -> Color:
+	return NamedColorList.color_list.pick_random()[0]
--- main.gd
+++ main.gd
@@ -36,7 +36,8 @@ func _ready() -> void:
 
 	if DecoTowerCount != 0:
 		orbitr *= 2
-	$DecoOrbit.init(orbitr)
+	var WorldSize :Vector3 = default_maze3d_setting.CalcSizeV3() * 4
+	$DecoOrbit.init(WorldSize, 9)
 	$AxisArrow3D.set_size(default_maze3d_setting.CalcDiagonalLengthV2()/2).set_colors()
 
 	$FixedCameraLight.set_center_pos_far(Vector3.ZERO, 	Vector3(0, 0, orbitr), orbitr*2)

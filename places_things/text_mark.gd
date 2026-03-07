a8cdf55fd311b08973199f877c9905cc0f328de3
seukwonkang <kasworld@gmail.com>
2026-03-07 15:07:29 +0900

    code clean

--- places_things/text_mark.gd
+++ places_things/text_mark.gd
@@ -1,27 +1,20 @@
-extends Node3D
+extends MeshInstance3D
 class_name TextMark
 
 var font = preload("res://font/HakgyoansimBareondotumR.ttf")
-var text :String
 
-func init(fsize :float, fdepth :float, co:Color, atext :String) -> TextMark:
-	text = atext
-	var mat := StandardMaterial3D.new()
-	mat.albedo_color = co
-	var mesh := TextMesh.new()
+func init(fsize :float, fdepth :float, co:Color, text :String) -> TextMark:
+	mesh = TextMesh.new()
 	mesh.font = font
 	mesh.depth = fdepth
 	mesh.pixel_size = fsize / 16
-	#mesh.font_size = fsize as int
 	mesh.text = text
-	mesh.material = mat
-	var sp := MeshInstance3D.new()
-	sp.mesh = mesh
-	add_child(sp)
+	mesh.material = StandardMaterial3D.new()
+	mesh.material.albedo_color = co
 	return self
 
 func get_text() -> String:
-	return text
+	return mesh.text
 
 var auto_rotate :bool = true
 func _process(delta: float) -> void:

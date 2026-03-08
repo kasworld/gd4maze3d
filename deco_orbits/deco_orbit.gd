afeea813bb95f4ccf9ef5cfc2bc57f9f65140e3d
seukwonkang <kasworld@gmail.com>
2026-03-08 11:15:41 +0900

    update deco orbit

--- deco_orbits/deco_orbit.gd
+++ deco_orbits/deco_orbit.gd
@@ -13,7 +13,7 @@ func add_orbitsphere(i :int, count :int) -> void:
 	var a30 := PI/6
 	var axis1 := Vector3.UP.rotated(
 		[Vector3.RIGHT, Vector3.LEFT, Vector3.FORWARD, Vector3.BACK].pick_random(),
-		a30)
+		randf_range(0,a30))
 	var 궤도mat1 := StandardMaterial3D.new()
 	궤도mat1.albedo_color = random_color()
 	var 구mat2
@@ -28,8 +28,8 @@ func add_orbitsphere(i :int, count :int) -> void:
 			구mat2 = StandardMaterial3D.new()
 			구mat2.albedo_color = random_color()
 	var os = preload("res://orbit_sphere/orbit_sphere.tscn").instantiate(
-		).궤도설정(diagonal_length, diagonal_length/200, axis1, a120*[0,1,2].pick_random()
-		).구설정(WorldSize.x/400*i+1, WorldSize.x/50, Vector3.UP
+		).궤도설정(diagonal_length, randfn(0,diagonal_length/100), axis1, a120*[0,1,2].pick_random()
+		).구설정(WorldSize.x/30*rate, WorldSize.x/50, Vector3.UP
 		).구재질설정(구mat2).궤도재질설정(궤도mat1)
 	add_child(os)
 	orbitsphere_list.append(os)

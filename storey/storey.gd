5bbc184344169b5eeef62d019b0b07044035d90b
seukwonkang <kasworld@gmail.com>
2026-05-25 09:20:10 +0900

    use wirenet for stair

--- storey/storey.gd
+++ storey/storey.gd
@@ -74,7 +74,7 @@ func init(num :int, grid_size := GridSize, cell_size := CellSize) -> Storey:
 	var floor_ceiling_height :float = cell_size.y *0.01
 	maze3d = preload("res://maze_3d/maze_3d.tscn").instantiate()
 	maze3d.init_params(maze2d, cell_size, cell_size.y *0.05, 1.0/(grid_size.x*grid_size.y))
-	maze3d.init_floor_ceiling(grid_size*4, floor_ceiling_height, 0.9,
+	maze3d.init_floor_ceiling(grid_size, floor_ceiling_height, 0.9,
 		Color(NamedColors.random_color(), 0.5),
 		Color(NamedColors.random_color(), 0.5),
 	)
@@ -98,10 +98,13 @@ func init(num :int, grid_size := GridSize, cell_size := CellSize) -> Storey:
 	구석자리목록.shuffle()
 	start_pos = 구석자리목록.pop_front()
 	goal_pos = 구석자리목록.pop_front()
-	#maze3d.make_stair(maze3d.get_floor(), start_pos, maze2d.get_open_dir_at(start_pos.x,start_pos.y).pick_random())
-	#maze3d.make_stair(maze3d.get_ceiling(), goal_pos, Maze.DirOpppsite[maze2d.get_open_dir_at(goal_pos.x,goal_pos.y).pick_random()])
+	# floor
 	maze3d.add_stair( Vector3i(start_pos.x, -1, start_pos.y) , maze2d.get_open_dir_at(start_pos.x,start_pos.y).pick_random(), NamedColors.random_color() )
+	maze3d.get_floor().del_at(Vector3i(start_pos.x, 0, start_pos.y))
+
+	# ceiling
 	maze3d.add_stair(Vector3i(goal_pos.x, 0, goal_pos.y), Maze.DirOpppsite[maze2d.get_open_dir_at(goal_pos.x,goal_pos.y).pick_random()], NamedColors.random_color() )
+	maze3d.get_ceiling().del_at(Vector3i(goal_pos.x, 0, goal_pos.y))
 
 	var 크기기준 :float = min(maze3d.calc_grid.unit_size.x, maze3d.calc_grid.unit_size.y,maze3d.calc_grid.unit_size.z)
 	$StartMark.init(크기기준*0.2, 크기기준/100, NamedColors.random_color(), "Start %d" % storey_num, start_pos

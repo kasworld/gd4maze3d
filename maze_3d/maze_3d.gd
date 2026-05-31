26267ec549182d5785016fd0bdcb86266d4abf0d
seukwonkang <kasworld@gmail.com>
2026-05-31 15:12:57 +0900

    add ladder

--- maze_3d/maze_3d.gd
+++ maze_3d/maze_3d.gd
@@ -124,8 +124,25 @@ func add_stair(cell_posi :Vector3i, dir :Maze.Dir, co :Color) -> WireNet:
 	add_child(wn)
 	return wn
 
+func add_ladder(cell_posi :Vector3i, dir :Maze.Dir, co :Color) -> WireNet:
+	var wn :WireNet = preload("res://wire_net/wire_net.tscn").instantiate()
+	wn.init(
+		Vector2(calc_grid.unit_size.x*0.5, calc_grid.unit_size.y),
+		Vector2i(0,6),
+		calc_grid.unit_size.y/30,
+		calc_grid.unit_size.y/30,
+		co,
+		false)
+	wn.position = calc_grid.posi_to_lanepos(cell_posi)
+	#wn.wire_H_rotation_x = PI/4
+	#wn.rotation.x = -PI/4
+	wn.rotation.y = Maze.DirToRadian(dir)
+	add_child(wn)
+	return wn
+
+
 func calc_tile_count(tg :Plot3D) -> Vector2i:
-	return Vector2( tg.calc_grid.grid_size.x / maze_cells.width, tg.calc_grid.grid_size.z / maze_cells.height)
+	return Vector2i( tg.calc_grid.grid_size.x / maze_cells.width, tg.calc_grid.grid_size.z / maze_cells.height)
 
 func make_stair_hole(tg :Plot3D, cell_posi :Vector2i) -> void:
 	var tile_count := calc_tile_count(tg)

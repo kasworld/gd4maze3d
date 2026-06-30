7ccf60f42a43563af384a9ebc0b438c658525e4e
seukwonkang <kasworld@gmail.com>
2026-06-30 11:45:33 +0900

    update PropTable4Leg

--- prop_wire_net/prop_wire_net.gd
+++ prop_wire_net/prop_wire_net.gd
@@ -3,22 +3,13 @@ class_name PropWireNet
 
 ## x-y wire net, face z+
 
-static func make_csg_box(box_size :Vector3, box_mat :StandardMaterial3D) -> CSGShape3D:
-	var box := CSGBox3D.new()
-	box.size = box_size
-	box.material = box_mat
-	return box
-
-static func MakeColorMaterial(co :Color, transparent :bool = false) -> StandardMaterial3D:
-	var material := StandardMaterial3D.new()
-	material.albedo_color = co
-	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA if transparent else BaseMaterial3D.TRANSPARENCY_DISABLED
-	return material
-
-## make dummy center
-static func MakeDummyCenter() -> CSGShape3D:
-	var center := make_csg_box(Vector3.ONE/1000, MakeColorMaterial(Color(0,0,0,0), true) )
-	return center
+func init(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, transparent :bool = false) -> PropWireNet:
+	var mat := MakeColorMaterial(co,transparent)
+	var center := MakeDummyCenter()
+	center = AddHWire(center, net_size,grid_count,wire_width, wire_height, mat)
+	center = AddVWire(center, net_size,grid_count,wire_width, wire_height, mat)
+	bake.call_deferred(center)
+	return self
 
 ## add H wire
 static func AddHWire(center :CSGShape3D, net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, mat :StandardMaterial3D, rot := Vector3.ZERO) -> CSGShape3D:
@@ -27,7 +18,7 @@ static func AddHWire(center :CSGShape3D, net_size :Vector2, grid_count :Vector2i
 	var unit_y := net_size.y/(grid_count.y-1) if grid_count.y > 1 else 0.0
 	for i in grid_count.y:
 		var pos := Vector3(0, -net_size.y/2 + unit_y *i, 0)
-		var wire := make_csg_box(h_wire_size, mat)
+		var wire := MakeCSGBox(h_wire_size, mat)
 		wire.operation = CSGShape3D.OPERATION_UNION
 		wire.position = pos
 		wire.rotation = rot
@@ -41,20 +32,29 @@ static func AddVWire(center :CSGShape3D, net_size :Vector2, grid_count :Vector2i
 	var unit_x := net_size.x/(grid_count.x-1) if grid_count.x > 1 else 0.0
 	for i in grid_count.x:
 		var pos := Vector3(-net_size.x/2 + unit_x *i, 0, 0)
-		var wire := make_csg_box(v_wire_size, mat)
+		var wire := MakeCSGBox(v_wire_size, mat)
 		wire.operation = CSGShape3D.OPERATION_UNION
 		wire.position = pos
 		wire.rotation = rot
 		center.add_child(wire)
 	return center
 
-func init(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, transparent :bool = false) -> PropWireNet:
-	var mat := MakeColorMaterial(co,transparent)
-	var center := MakeDummyCenter()
-	center = AddHWire(center, net_size,grid_count,wire_width, wire_height, mat)
-	center = AddVWire(center, net_size,grid_count,wire_width, wire_height, mat)
-	bake.call_deferred(center)
-	return self
-
 func bake(csg :CSGShape3D) -> void:
 	$MeshInstance3D.mesh = csg.bake_static_mesh()
+
+static func MakeCSGBox(box_size :Vector3, box_mat :StandardMaterial3D) -> CSGShape3D:
+	var box := CSGBox3D.new()
+	box.size = box_size
+	box.material = box_mat
+	return box
+
+static func MakeColorMaterial(co :Color, transparent :bool = false) -> StandardMaterial3D:
+	var material := StandardMaterial3D.new()
+	material.albedo_color = co
+	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA if transparent else BaseMaterial3D.TRANSPARENCY_DISABLED
+	return material
+
+## make dummy center
+static func MakeDummyCenter() -> CSGShape3D:
+	var center := MakeCSGBox(Vector3.ONE/1000, MakeColorMaterial(Color(0,0,0,0), true) )
+	return center
